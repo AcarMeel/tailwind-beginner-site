43e3d6f9b39cae973c19281f2d1aa92453ccf62f
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2020-12-28 18:51:19 -0600

    changing versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["src/**/*.html"],
+//   purge: ["src/**/*.html"],
   theme: {
     extend: {
+      colors: {
         teal: colors.teal,
-        orange: colors.orange
+        orange: colors.orange,
+      },
     },
   },
   variants: {},
